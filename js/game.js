d11403d3cba13587b824f86eb66ec07322379fa9
Erik Nygren <erik.nygren6@gmail.com>
2015-12-10 21:44:42 +0000

    You can now play the board! (but without any check for a winner)

--- js/game.js
+++ js/game.js
@@ -13,20 +13,34 @@ var NC = {};
     this.board = _.map(_.range(3), function(){ 
       return new NC.Row();
     });
-  }
+  };
+
+  NC.Game.prototype.makeMove = function(rowIndex, tileIndex) {
+    if (!!this.board[rowIndex].tiles[tileIndex].checkedID) {
+      //tile already checked
+      return;
+    }
+    this.board[rowIndex].tiles[tileIndex].checkedID = this.currentPlayerTurnIndex;
+    this.moveToNextPlayer();
+  };
+
+  NC.Game.prototype.moveToNextPlayer = function() {
+    var current = this.currentPlayerTurnIndex;
+    this.currentPlayerTurnIndex = current == 0 ? 1 : 0;
+  };
 
   NC.Row = function () {
     this.tiles = _.map(_.range(3), function(){ 
       return new NC.Tile();
     });
-  }
+  };
 
   NC.Tile = function() {
     this.checkedID = null;
-  }
+  };
 
   NC.Player = function() {
     this.wins = 0;
-  }
+  };
 
 })();
